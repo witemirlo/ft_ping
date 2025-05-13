c98430b0d9b0afdc664af7aae8ada8c254e39f14
witemirlo <jacoboberdugo@gmail.com>
2025-05-13 13:39:50 +0200

    clean comments

--- src/main.c
+++ src/main.c
@@ -21,22 +21,8 @@ int main(int argc, char* argv[])
 	t_flags flags = get_flags(argc, argv);
 
 	t_addr data = {0};
-	int sockfd = get_socket(argv[1], &data);
+	const int sockfd = get_socket(argv[1], &data);
 	printf("%s:%d: %s: %s (%d.%d.%d.%d)\n", __FILE__, __LINE__, __func__, data.canonname, (data.addr & 0xff), ((data.addr >> 8) & 0xff), ((data.addr >> 16) & 0xff), ((data.addr >> 24) & 0xff)); // TODO: el endianess
-	// int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
-
-	if (sockfd < 0) {
-		fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); // TODO: BORRAR
-		fprintf(stderr, "%s: Error: %s\n", __progname, strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	int opt = 1;
-	if (setsockopt(sockfd, IPPROTO_IP, IP_RECVERR, &opt, sizeof(opt)) < 0) {
-		fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); // TODO: BORRAR
-		fprintf(stderr, "%s: Error: %s\n", __progname, strerror(errno));
-		return EXIT_FAILURE;
-	}
 
 	struct icmp icmp = {0};
 	icmp.icmp_type = ICMP_ECHO;
@@ -48,9 +34,7 @@ int main(int argc, char* argv[])
 	icmp.icmp_cksum = sum_ones_complement(icmp.icmp_cksum, icmp.icmp_hun.ih_idseq.icd_id);
 	icmp.icmp_cksum = sum_ones_complement(icmp.icmp_cksum, icmp.icmp_hun.ih_idseq.icd_seq);
 
-	// printf("%s:%d: %s: %x\n", __FILE__, __LINE__, __func__, icmp.icmp_cksum);
 	icmp.icmp_cksum = 0xffff - icmp.icmp_cksum;
-	// printf("%s:%d: %s: %x\n", __FILE__, __LINE__, __func__, icmp.icmp_cksum);
 	
 	struct sockaddr_in tmp2 = {0};
 	socklen_t tmp22 = sizeof(tmp2);
@@ -68,8 +52,6 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-	// if (sendto(sockfd, &tmp, sizeof(tmp), 0, (struct sockaddr*)&tmp2, tmp22) < 0) {
-
 	char buffer[BUFSIZ] = {0};
 	clock_t start = clock();
 	if (sendto(sockfd, &icmp, sizeof(icmp), 0, (struct sockaddr*)&tmp2, tmp22) < 0) {
@@ -78,39 +60,14 @@ int main(int argc, char* argv[])
 		return EXIT_FAILURE;
 	}
 
-
-
-	// struct icmp prueba = {0};
-	// if (recvfrom(sockfd, &prueba, sizeof(prueba), 0, (struct sockaddr*)&tmp2, &tmp22) <= 0) {
 	if (recvfrom(sockfd, &buffer, sizeof(buffer), 0, (struct sockaddr*)&tmp2, &tmp22) <= 0) {
 		fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); // TODO: BORRAR
 		fprintf(stderr, "%s: Error: %s\n", __progname, strerror(errno));
 		return EXIT_FAILURE;
 	}
-
-	// printf("%d\n",prueba.icmp_type);
-	// printf("%d\n",prueba.icmp_code);
-
-	// char buffer1[BUFSIZ] = {0};
-	// char buffer2[BUFSIZ] = {0};
-	// struct msghdr msg = {
-	// 	.msg_name = &buffer1,
-	// 	.msg_namelen = BUFSIZ,
-	// 	.msg_control = &buffer2,
-	// 	.msg_controllen = BUFSIZ,
-	// 	.msg_iov = 0,
-	// 	.msg_iovlen = 0,
-	// 	.msg_flags = 0
-	// };
-	// if (recvmsg(sockfd, &msg, 0) < 0) {
-	// 	fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); // TODO: BORRAR
-	// 	fprintf(stderr, "%s: Error: %s\n", __progname, strerror(errno));
-	// 	return EXIT_FAILURE;
-	// }
+	
 	clock_t end = clock();
 	printf("time: %f\n", (((double)(end - start)) / CLOCKS_PER_SEC) * 100000);
-
-	// printf("%d\n", ((struct cmsghdr*)(msg.msg_control))->cmsg_level);
 
 	return EXIT_SUCCESS;
 }
